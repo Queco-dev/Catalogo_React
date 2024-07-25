288792cc917add09f68b30efed142b68babe0c72
Queco-dev <lindyclaxon@alumnos.cei.es>
2024-07-25 10:39:02 +0200

    update

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,6 +1,6 @@
 
 
-const Productos = ({imagen,artist,id}) => {
+const Productos = ({imagen,artist,id,precio,medium}) => {
 
     //console.log()
     return ( 
@@ -8,10 +8,13 @@ const Productos = ({imagen,artist,id}) => {
         {/* <li key={fichita.id}>{fichita.artist}</li>
         <img src={fichita.imagen} alt={fichita.artist} /> */}
 
-        <li key={id}>{artist}</li>
         <img src={imagen} alt={artist} />
+        <li key={id}>{artist}</li>
+        <p><b>Tipo:</b>{medium}</p>
+        <p><b>Nombre:</b>{artist}</p>
+        <p><b>Precio:</b>{precio}</p>
         </>
      );
-}
+}   
  
 export default Productos;
